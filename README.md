1d04d39c6e5ee42913977918e117f6d67924bc49
CrakedOnYt <crakedjojo7@gmail.com>
2021-12-16 14:05:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vs-Cubethony-Source-Code
+ Coded by Me :3
---
+++ example_mods/stages/mall2.lua
@@ -0,0 +1,9 @@
+function onCreate()
+
+	makeLuaSprite('bg','mall2',-600,-300)
+	addLuaSprite('bg','false',-600,-300)
+	setLuaSpriteScrollFactor('bg', 0.9, 0.9);
+	
+	close(true)
+
+end
